10ea8c9996a56f88d3b24f3171f240689a14a5a4
SSM <blussm@kakao.com>
2023-12-22 15:15:31 +0900

    View 환경설정

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication.run(HelloSpringApplication.class, args); // SpringApplication.run(실행할 클래스명) 실행 시 Annotation에 의해 내장 톰캣을 통해 웹서버를 자체적으로 띄움
+		// IJ에서 자바를 통해 Build하지 않고 Gradle을 통해 빌드하는 케이스가 있으니 gradle 빌드 및 실행 설정을 IJ로 변경 
 	}
 
 }
--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("hello") // url에 입력된 path
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
-        return "hello";
+        return "hello"; // 렌더링 할 페이지 -> view Reslover가 resources:templates/ + {ViewName} + .html 파일 실행
     }
 }
